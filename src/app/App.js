14df2644591b993ca67a211d3b05c7cdbc67b0b2
Nikita Gutman <gut@d-element.ru>
2023-10-10 17:10:55 +0500

    add template engine

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,19 @@
 import "./style.pcss"
-import { worker } from "../shared/api/browser.js"
-if (process.env.NODE_ENV === "development") {
-  worker.start()
-}
-import { Header } from "/src/widgets/header"
-import { Footer } from "/src/widgets/footer";
-let pageContent
-import { getPage } from "../shared/lib/index.js";
-switch (window.location.pathname) {
-  case "/test":
-    pageContent = `<h1>Тест!</h1>`
-    break
-  case "/":
-    pageContent = `<h1>Главная!</h1>`
-    break
-  default:
-    pageContent = `<h1>404</h1>`
-}
-const nodeElement = getPage(Header, Footer, pageContent)
-document.querySelector("body").insertAdjacentHTML("afterbegin", nodeElement)
 
+const runApp = async () => {
+    switch (process.env.NODE_ENV) {
+        case "development":
+            await import("../shared/api/browser.js")
+                .then(async ({ worker }) => {
+                    await worker.start().then(() => {
+                        console.debug("App dev run")
+                    })
+                })
+
+    }
+}
+
+runApp()
+    .catch((err) => {
+        console.error(err)
+    })
--- src/shared/lib/index.js
+++ src/shared/lib/index.js
@@ -1,8 +1,17 @@
-
-export const getPage = (header, footer, nodeElement) => {
-  return `
-  ${header}
-  ${nodeElement}
-  ${footer}
-  `
+export const getPage = ({ body, title }) => {
+    return `
+      <!DOCTYPE html>
+      <html lang="ru">
+        <head>
+          <meta charset="UTF-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>${title}</title>
+          <script src="/src/app/App.js" defer type="module"></script>
+        </head>
+        <body>
+          ${body}
+        </body>
+      </html>
+    `
 }
